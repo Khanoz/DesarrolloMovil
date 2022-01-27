9db60b5cdb039abb920b26c5b9a037ea7caae9ab
gnds99 <noe.gnds.99@hotmail.com>
2022-01-26 20:37:27 -0500

    Finalización de la tercera practica
    Se agregue el tercer activity ya que olvide que eran tres

--- Practica03/app/src/main/java/com/example/practica03/MainActivity3.java
+++ Practica03/app/src/main/java/com/example/practica03/MainActivity3.java
@@ -7,31 +7,31 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
-public class MainActivity2 extends AppCompatActivity {
+public class MainActivity3 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main3);
     }
     @Override
     protected void onStart(){
         super.onStart();
-        Toast.makeText(this, "OnStart 2", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "OnStart 3", Toast.LENGTH_SHORT).show();
         // La actividad esta a punto de hacerse visible
     }
 
     @Override
     protected void onResume(){
         super.onResume();
-        Toast.makeText(this, "OnResume 2", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "OnResume 3", Toast.LENGTH_SHORT).show();
         // La actividad se ha vuelto visibld (ahora se "reanuda"
     }
 
     @Override
     protected void onPause(){
         super.onPause();
-        Toast.makeText(this, "OnPause 2", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "OnPause 3", Toast.LENGTH_SHORT).show();
         // Enfocarse en otra actividad (esta actividad esta a punto de ser "detenida")
         finish(); // termina el activity
     }
@@ -39,32 +39,26 @@ public class MainActivity2 extends AppCompatActivity {
     @Override
     protected void onStop(){
         super.onStop();
-        Toast.makeText(this, "OnStop 2", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "OnStop 3", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected  void onDestroy(){
         super.onDestroy();
-        Toast.makeText(this, "OnDestroy 2", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "OnDestroy 3", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onRestart(){
         super.onRestart();
-        Toast.makeText(this, "OnRestart 2", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "OnRestart 3", Toast.LENGTH_SHORT).show();
     }
 
     // Metodo para el boton anterior
     public void Anterior (View view){
-        Intent Anterior  = new Intent(this, MainActivity.class);
+        Intent Anterior  = new Intent(this, MainActivity2.class);
         startActivity(Anterior);
 
     }
 
-    // Metodo para el boton siguiente tercer activity
-    public void Siguiente (View view){
-        Intent Siguiente  = new Intent(this, MainActivity3.class);
-        startActivity(Siguiente);
-
-    }
 }
